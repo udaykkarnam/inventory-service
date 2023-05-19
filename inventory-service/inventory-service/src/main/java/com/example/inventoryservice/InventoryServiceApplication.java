b18eeabdf272e979827f28eeebb59a72c88953d3
Uday Kumar <udaykumarkarnam7@gmail.com>
2023-05-19 13:39:33 +0530

    commit inventory

---
+++ inventory-service/inventory-service/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,36 @@
+package com.example.inventoryservice;
+
+import com.example.inventoryservice.model.Inventory;
+import com.example.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository){
+		return args ->{
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("ïphone_13");
+			inventory.setQuantity(100);
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("ïphone_14");
+			inventory1.setQuantity(0);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory1);
+
+
+		};
+
+	}
+
+}
---
+++ inventory-service/src/main/java/com/example/inventoryservice/service/InventoryService.java
@@ -0,0 +1,21 @@
+package com.example.inventoryservice.service;
+
+import com.example.inventoryservice.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import net.bytebuddy.asm.Advice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+    public boolean isInStock(String skuCode){
+        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+
+    }
+}
